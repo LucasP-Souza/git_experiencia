effb34c471c891f31c5ab76c83f027188f0d66db
Lucas Paulo de Souza <lucas.paulo0107@gmail.com>
2024-05-27 15:26:54 -0300

    Typeof e como usar

---
+++ aula011-typeof.js
@@ -0,0 +1,9 @@
+let a = 10
+let b = '10'
+let c = a === b
+let d = {nome : "Bruno"}
+
+console.log(`Valor: ${a} - Tipo: ${typeof(a)}`)
+console.log(`Valor: ${b} - Tipo: ${typeof(b)}`)
+console.log(`Valor: ${c} - Tipo: ${typeof(c)}`)
+console.log(`Valor: ${d} - Tipo: ${typeof(d)}`)
