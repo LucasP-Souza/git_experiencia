ec599ec15955c904ebc36fd8c22f4fedf7015da1
Lucas Paulo de Souza <lucas.paulo0107@gmail.com>
2024-05-29 17:55:40 -0300

    Aprendendo sobre for in  e for of

---
+++ forinforof.js
@@ -0,0 +1,16 @@
+/*const listanomes = ['lucas', 'vitoria', 'scheila']
+const numeros = [10,20,30,40,50,60,50]
+for(n of numeros){
+    console.log(n)
+}
+
+for(n in numeros){
+    console.log(n)
+}*/
+let fat = 1
+let num = 5
+while(fat < 5){
+    num *= fat
+    fat++
+    console.log(num)
+}
